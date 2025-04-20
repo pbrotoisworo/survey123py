a8f319689691ddbe9ce03d22dbddd1bab29cb5da
Panji Brotoisworo <32087633+pbrotoisworo@users.noreply.github.com>
2025-04-20 20:11:07 +0800

    Add basic unit tests
    - Check YAML and templates are loaded as type pd.DataFrame

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,15 +1,32 @@
 import unittest
 from pathlib import Path
-from survey123py.form import FormData
+from survey123py.form import FormData, Sheets
+import pandas as pd
 
 class TestSurvey123_322(unittest.TestCase):
 
     def setUp(self):
         self.survey = FormData("3.22")
         self.test_file = Path(__file__).parent / "data" / "sample_survey_full.yaml"
+        self.sheet_names = Sheets
 
     def test_load_survey(self):
+        
         self.survey.load_yaml(self.test_file)
+
+        # All data from template and YAML must be loaded into a dataframe
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.survey], pd.DataFrame))
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.choices], pd.DataFrame))
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.settings], pd.DataFrame), f"Got {type(self.survey.sheets[self.sheet_names.settings])} {self.survey.sheets[self.sheet_names.settings]}")
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.version], pd.DataFrame))
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.question_types], pd.DataFrame))
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.appearances], pd.DataFrame))
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.field_types], pd.DataFrame))
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.reference], pd.DataFrame))
+        self.assertTrue(isinstance(self.survey.sheets[self.sheet_names.reserved], pd.DataFrame))
+
+        # TODO: Verify dataframe values
+
 
     def test_save_survey(self):
         self.survey.load_yaml(self.test_file)

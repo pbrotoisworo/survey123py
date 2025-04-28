d4ff9913ed36a68ce2c6a3505a5b03fdc1bcef77
Panji Brotoisworo <32087633+pbrotoisworo@users.noreply.github.com>
2025-04-28 12:20:15 +0800

    Fix parsing logic (now working)
    - works with nested items and non-nested items

--- survey123py/preview.py
+++ survey123py/preview.py
@@ -1,8 +1,9 @@
 import re
+import yaml
 
 class FormPreviewer:
 
-    def __init__(self, yaml: str):
+    def __init__(self, yaml_path: str):
         """
         Test the output of a form by loading the YAML file and generating a preview of the form output.
         Note: this requires the yaml file to contain sample input using the column `survey123py::preview_input`
@@ -22,11 +23,11 @@ class FormPreviewer:
             Path to the YAML file containing survey data.
         """
         # Load YAML file
-        with open(yaml, 'r') as file:
+        with open(yaml_path, 'r') as file:
             self.yaml_data = yaml.safe_load(file)
             # Create copy for output
             self.output_data = self.yaml_data.copy()
-        self.ctx = self._load_ctx(self.yaml_data)
+        self.ctx = self._load_ctx()
 
     def _load_ctx(self) -> dict:
         """
@@ -56,26 +57,48 @@ class FormPreviewer:
         """
         # This pattern matches ${var_name} in the string
         pattern = r"\$\{(\w+)\}"
-
+        
         for i, item in enumerate(survey_data["survey"]):
+
+            # Separate loop to handle nested children items (groups, repeaters)
             if item["type"] == "group" or item["type"] == "repeat":
                 for item2 in item["children"]:
-                    if key not in ["type", "name"]:
-                        for key, value in item2.items():
+                    for key, value in item2.items():
+                        if key not in ["type", "name", "survey123py::preview_input"]:
                             if isinstance(value, bool):
                                 continue
                             matches = re.findall(pattern, value)
                             if matches:
+                                for match in matches:
+                                    if match in self.ctx:
+                                        # Replace ${} variable with variable in data ctx
+                                        survey_data["survey"][i]["children"][0][key] = item2[key].replace("${" + match + "}", str(self.ctx[match]))
+            
+            for key, value in item.items():
+                if key not in ["type", "name", "survey123py::preview_input", "children"]:
+                    matches = re.findall(pattern, value)
+                    if matches:
+                        for match in matches:
+                            if match in self.ctx:
                                 # Replace ${} variable with variable in data ctx
-                                survey_data["survey"][i]["children"][0][key] = item2[key].replace("${" + matches[0] + "}", self.ctx[matches[0]])
-                continue
-        for key, value in item.items():
-            if key not in ["type", "name"]:
-                matches = re.findall(pattern, value)
-                if matches:
-                    # Replace ${} variable with variable in data ctx
-                    survey_data["survey"][i][key] = item[key].replace("${" + matches[0] + "}", self.ctx[matches[0]])
+                                survey_data["survey"][i][key] = item[key].replace("${" + match + "}", str(self.ctx[match]))
         return survey_data
+    
+    def show_preview(self):
+
+        """
+        Generate a preview of the form output by parsing the YAML file and replacing variable references with their values.
+
+        Returns
+        -------
+        dict
+            The parsed survey data with variable references replaced by their values.
+        """
+        # Parse variables in the survey data
+        self.output_data = self._parse_vars(self.output_data)
+        
+        # Return the parsed survey data
+        return self.output_data
     
     def _eval():
         """
